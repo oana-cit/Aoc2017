c12f6c7d08b65a00ac595669fc635aa3b3d3af5e
Oana Citoteanu <ocitoteanu@ixiacom.com>
2017-12-06 15:00:08 +0200

    9 stars.

--- adventOfCode/Day3_2.py
+++ adventOfCode/Day3_2.py
@@ -17,74 +17,67 @@ def getAdjSquareSum():
     adjSquareSum += valMatrix[maxRange + posX + 1][maxRange + posY - 1]
     return adjSquareSum
 
-def getNewCoord(step):
-    global posX, posY, theirNumber, crtNumber, valMatrix
-    
-    if step > 0:
-        return theirNumber - crtNumber
-    return crtNumber - theirNumber
+def check():
+    global posX, posY, theirNumber, maxRange, valMatrix
+    found = valMatrix[maxRange + posX][maxRange + posY] > theirNumber 
+    if found:
+        print("[{}][{}] +++> {}".format(posX, posY, valMatrix[maxRange + posX][maxRange + posY]))
+    return found
 
 def checkAndUpdate():
-    global posX, posY, theirNumber, maxRange
+    global posX, posY, theirNumber, maxRange, valMatrix
     crtSum = getAdjSquareSum()
     if crtSum == 0:
         crtSum = 1
-        
-    print("posX: {}, posY: {}, val: {}".format(posX, posY, crtSum))
-        
-    valMatrix[maxRange + posX][maxRange + posY] = crtSum
-    return crtSum > theirNumber
+    
+    valMatrix[maxRange + posX][maxRange + posY] = crtSum    
+    print("[{}][{}] => {}".format(posX, posY, crtSum))
+    
+    return check()
 
 def goSpiraling(stepX, stepY):
-    global posX, posY, theirNumber, crtNumber
-#     
-#     if theirNumber < crtNumber + abs(stepX):
-#         stepX = getNewCoord(stepX)
-#     
-#     if theirNumber < crtNumber + abs(stepY):
-#         stepY = getNewCoord(stepY)
-    
+    global posX, posY, theirNumber, maxRange, valMatrix
+ 
     finished = False
     while stepX > 0 and not finished:
-        finished = checkAndUpdate()
+        print("stepX > 0 : stepX: {}, posX: {}".format(stepX, posX))
         posX += 1
         stepX -= 1
-    while stepX < 0 and not finished:
         finished = checkAndUpdate()
+#         finished = True
+    while stepX < 0 and not finished:
+        print("stepX < 0 : stepX: {}, posX: {}".format(stepX, posX))
         posX -= 1
         stepX += 1
-    while stepY > 0 and not finished:
         finished = checkAndUpdate()
+#         finished = True
+    while stepY > 0 and not finished:
+        print("stepY > 0 : stepY: {}, posY: {}".format(stepY, posY))
         posY += 1
         stepY -= 1
-    while stepY < 0 and not finished:
         finished = checkAndUpdate()
+#         finished = True
+    while stepY < 0 and not finished:
+        print("stepY < 0 : stepY: {}, posY: {}".format(stepY, posY))
         posY -= 1
         stepY += 1
-        
-#     posX += stepX
-#     posY += stepY
-    crtNumber += abs(stepX) + abs(stepY)
+        finished = checkAndUpdate()
+#         finished = True
+   
+    if valMatrix[maxRange + posX][maxRange + posY] > theirNumber:
+        return check()
     
-    if valMatrix[maxRange + posX][maxRange + posY] > theirNumber:    
-        print("reached {} with posX: {}, posY: {}, distance: {}".format(theirNumber, posX, posY, abs(posX) + abs(posY)))
-        print("valMatrix: {}".format(valMatrix))
-        print("first value written that is larger: {}".format(valMatrix[maxRange + posX][maxRange + posY]))
-        return True
     return False
 
 
 print("Let us begin!")
-# theirNumber = 265149  # 438
-theirNumber = 1 #0
-# theirNumber = 12 #3
-# theirNumber = 23 #2
-# theirNumber = 1024 #31
+theirNumber = 265149  # 438
+# theirNumber = 1 #0
+# theirNumber = 747
 valMatrix = [[0 for col in range(200)] for row in range(200)]
 maxRange = 100
 maxNo = 1
 crtStep = 0
-lineMax = []
 posX = 0
 posY = 0
 finished = False
@@ -94,24 +87,17 @@ valMatrix[maxRange + posX][maxRange + posY] = 1
 
 while not finished:
     finished = goSpiraling(1, 0)
-    print("crtNumber {} with posX: {}, posY: {}".format(valMatrix[maxRange + posX][maxRange + posY], posX, posY))
     crtStep += 1
-    
-    finished = goSpiraling(0, crtStep)
-    
-    print("crtNumber {} with posX: {}, posY: {}".format(valMatrix[maxRange + posX][maxRange + posY], posX, posY))
+    if not finished:
+        finished = goSpiraling(0, crtStep)
     
     crtStep += 1
-    finished = goSpiraling(-crtStep, 0)
-    print("crtNumber {} with posX: {}, posY: {}".format(valMatrix[maxRange + posX][maxRange + posY], posX, posY))
-    finished = goSpiraling(0, -crtStep)
-    print("crtNumber {} with posX: {}, posY: {}".format(valMatrix[maxRange + posX][maxRange + posY], posX, posY))
-    finished = goSpiraling(crtStep, 0)
-    print("crtNumber {} with posX: {}, posY: {}".format(valMatrix[maxRange + posX][maxRange + posY], posX, posY))
+    if not finished:
+        finished = goSpiraling(-crtStep, 0)
+    if not finished:
+        finished = goSpiraling(0, -crtStep)
+    if not finished:
+        finished = goSpiraling(crtStep, 0)
     
-    lineMax.append(valMatrix[maxRange + posX][maxRange + posY])
-    print("Crt progress on step {}: {}".format(valMatrix[maxRange + posX][maxRange + posY], lineMax))
-    
-#     posY += 1
-#         maxNo += 4 * (crtLength - 1)
+    print("[{}][{}] ==>> {}".format(posX, posY, valMatrix[maxRange + posX][maxRange + posY]))
       
